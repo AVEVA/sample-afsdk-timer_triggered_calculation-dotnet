05fa9f11dbb51d8a4e37841bfe486668a3c3d7d2
Kelsey Duffy <kduffy@osisoft.com>
2021-07-13 16:53:22 -0400

    unit tests passing locally

--- TimerTriggeredCalc/Program.cs
+++ TimerTriggeredCalc/Program.cs
@@ -1,25 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Timers;
 using OSIsoft.AF.Asset;
 using OSIsoft.AF.Data;
 using OSIsoft.AF.PI;
+using Timer = System.Timers.Timer;
 
 namespace TimerTriggeredCalc
 {
     public static class Program
     {
-        private static Timer aTimer;
-        private static PIPoint output;
-        private static PIPoint input;
+        private static Timer _aTimer;
+        private static PIPoint _output;
+        private static PIPoint _input;
 
-        public static void Main(string[] args)
+        public static void Main()
+        {
+            var success = MainLoop(false);
+        }
+
+        public static bool MainLoop(bool test = false)
         {
             #region configuration
 
             var inputTagName = "cdt158";
             var outputTagName = "cdt158_output_timerbased";
             var timerMS = 60000; // how long to pause between cycles, in ms
+            var startOnTheMinute = true; // start the calculation exactly on the minute
 
             #endregion // configuration
 
@@ -29,32 +38,48 @@ namespace TimerTriggeredCalc
             // Get or create the output PI Point
             try
             {
-                output = PIPoint.FindPIPoint(myServer, outputTagName);
+                _output = PIPoint.FindPIPoint(myServer, outputTagName);
             }
             catch (PIPointInvalidException)
             {
-                output = myServer.CreatePIPoint(outputTagName);
-                output.SetAttribute(PICommonPointAttributes.PointType, PIPointType.Float64);
+                _output = myServer.CreatePIPoint(outputTagName);
+                _output.SetAttribute(PICommonPointAttributes.PointType, PIPointType.Float64);
             }
 
             // Resolve input tag name to PIPoint object
-            input = PIPoint.FindPIPoint(myServer, inputTagName);
+            _input = PIPoint.FindPIPoint(myServer, inputTagName);
+
+            // Optionally pause the program until the top of the next minute
+            if (startOnTheMinute)
+            {
+                DateTime now = DateTime.Now;
+                Thread.Sleep(((60 - now.Second) * 1000) - now.Millisecond);
+            }
 
             // Create a timer with the specified interval
-            aTimer = new Timer();
-            aTimer.Interval = timerMS;
+            _aTimer = new Timer();
+            _aTimer.Interval = timerMS;
 
             // Add the calculation to the timer's elapsed trigger event handler list
-            aTimer.Elapsed += PerformCalculation;
+            _aTimer.Elapsed += PerformCalculation;
 
             // Enable the timer and have it reset on each trigger
-            aTimer.AutoReset = true;
-            aTimer.Enabled = true;
+            _aTimer.AutoReset = true;
+            _aTimer.Enabled = true;
 
-            // Allow the program to run indefinitely
-            Console.WriteLine($"Calculations are executing every {timerMS} ms. Press <ENTER> to end... ");
-            Console.ReadLine();
+            // Allow the program to run indefinitely if not being tested
+            if (!test)
+            {
+                Console.WriteLine($"Calculations are executing every {timerMS} ms. Press <ENTER> to end... ");
+                Console.ReadLine();
+            }
+            else
+            {
+                // Pause to let the calculation run twice before ending the test
+                Thread.Sleep(timerMS * 2);
+            }
 
+            return true;
         }
 
         private static void PerformCalculation(object source, ElapsedEventArgs e)
@@ -64,13 +89,17 @@ namespace TimerTriggeredCalc
             var numStDevs = 1.75; // number of standard deviations of variance to allow
 
             // Obtain the recent values from the trigger timestamp
-            var afvals = input.RecordedValuesByCount(e.SignalTime, numValues, false, AFBoundaryType.Interpolated, null, false);
+            var afvals = _input.RecordedValuesByCount(e.SignalTime, numValues, false, AFBoundaryType.Interpolated, null, false);
 
             // Remove bad values
             var badItems = new List<AFValue>();
             foreach (var afval in afvals)
+            {
                 if (!afval.IsGood)
+                {
                     badItems.Add(afval);
+                }
+            }
 
             foreach (var item in badItems)
                 afvals.Remove(item);
@@ -98,8 +127,12 @@ namespace TimerTriggeredCalc
                 var itemsToRemove = new List<AFValue>();
 
                 foreach (var afval in afvals)
+                {
                     if (Math.Abs(afval.ValueAsDouble() - avg) > cutoff)
+                    {
                         itemsToRemove.Add(afval);
+                    }
+                }
 
                 // If there are items to remove, remove them and loop again
                 if (itemsToRemove.Count > 0)
@@ -107,13 +140,13 @@ namespace TimerTriggeredCalc
                     foreach (var item in itemsToRemove)
                         afvals.Remove(item);
                 }
+
                 // If not, write the average to the output tag and break the loop
                 else
                 {
-                    output.UpdateValue(new AFValue(avg, e.SignalTime), AFUpdateOption.Insert);
+                    _output.UpdateValue(new AFValue(avg, e.SignalTime), AFUpdateOption.Insert);
                     break;
                 }
-
             }
         }
     }
---
+++ TimerTriggeredCalcTests/UnitTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TimerTriggeredCalcTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TimerTriggeredCalcTest()
+        {
+            Assert.IsTrue(TimerTriggeredCalc.Program.MainLoop(true));
+        }
+    }
+}
