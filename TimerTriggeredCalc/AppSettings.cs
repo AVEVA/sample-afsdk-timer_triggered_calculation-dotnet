669672476ffa1ce1f936b4aea8f1bf7e3433572a
Collin Bardini <cbardini@osisoft.com>
2022-02-14 13:43:41 -0800

    Update AppSettings.cs

--- TimerTriggeredCalc/AppSettings.cs
+++ TimerTriggeredCalc/AppSettings.cs
@@ -41,5 +41,17 @@ namespace TimerTriggeredCalc
         /// The number of seconds to offset from the top of the minute for the first calculation
         /// </summary>
         public int OffsetSeconds { get; set; }
+
+        /// <summary>
+        /// The username to use when connecting to AF
+        /// An empty string will use the application user
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// The password to use when connecting to AF
+        /// An empty string will use the application user
+        /// </summary>
+        public string Password { get; set; }
     }
 }
